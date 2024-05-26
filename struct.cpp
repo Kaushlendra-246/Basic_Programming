c5ae0f40704f9b052d7522cc09e0f516361b9ebe
Kaushlendra-246 <kkc2857573@gmail.com>
2024-05-26 08:10:38 +0530

    Doubt

--- struct.cpp
+++ struct.cpp
@@ -19,10 +19,10 @@ struct Node{
 
 int main(){
     vector<int> v = {1,5,3,2,8};
-    Node y = Node(v[0],NULL);
+    Node y = Node(v[0],NULL); //y is just a variable of type Node
     cout << y.data << " " << y.next << endl;
 
     //difference
-    Node* x = new Node(v[1]);
-    cout << y.data << " " << y.next << endl;
+    Node* x = new Node(v[1]); // x is a pointer
+    cout << x->data << " " << x->next << endl;
 }
