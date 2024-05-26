00ed9adb23c83f49971adc10162eee624dc2abeb
Kaushlendra-246 <kkc2857573@gmail.com>
2024-05-26 07:51:49 +0530

    Doubt

---
+++ struct.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+
+    // //Constructer in C language show error
+    // struct Node(int data, struct Node* next){
+        
+    // } 
+};
+
+int main(){
+    
+}
